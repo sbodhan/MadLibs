aa592f7823f576d0ba726e0742e31269ca68515b
Srinivas Bodhanampati <sbodhan@srinivass-macbook-pro.local>
2016-05-24 14:44:02 -0400

    Initial Commit

---
+++ MadLibs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MadLibs
+//
+//  Created by Srinivas Bodhanampati on 5/24/16.
+//  Copyright © 2016 Srinivas Bodhanampati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MadLibs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MadLibs
+//
+//  Created by Srinivas Bodhanampati on 5/24/16.
+//  Copyright © 2016 Srinivas Bodhanampati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
